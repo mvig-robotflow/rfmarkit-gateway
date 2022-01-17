daf16179a268929ab07832cdca2855d9e0e4a3ad
davidliyutong <davidliyutong@sjtu.edu.cn>
2022-01-18 07:16:09 +0800

    Use select to increase throughput

--- tcp_broker/helpers.py
+++ tcp_broker/helpers.py
@@ -1,5 +1,6 @@
-from typing import Dict, BinaryIO
 import socket
+from typing import Dict, BinaryIO, List
+
 
 def insert_data(f: BinaryIO, data: bytes):
     """Insert data to FileIO database (file)
@@ -12,16 +13,21 @@ def insert_data(f: BinaryIO, data: bytes):
         return
     f.write(data)
 
-def unregister_fd(fd: int, client_sockets: Dict[int, socket.socket], file_handles: Dict[int, BinaryIO]):
+
+def unregister_fd(fd: int,
+                  client_sockets: Dict[int, socket.socket],
+                  client_read_fds: List[int],
+                  file_handles: Dict[int, BinaryIO]):
     """Unregister file descriptor
 
     Args:
         fd (int): [description]
         client_sockets (Dict[int, socket.socket]): [description]
+        client_read_fds:
         file_handles (Dict[int, BinaryIO]): [description]
     """
     client_sockets[fd].close()
     file_handles[fd].close()
+    client_read_fds.remove(fd)
     del client_sockets[fd]
     del file_handles[fd]
-
--- tcp_broker/tasks/tcp_process.py
+++ tcp_broker/tasks/tcp_process.py
@@ -4,15 +4,18 @@ import os
 import socket
 import time
 from typing import Any, Dict, BinaryIO
+
+import select
+
 from config import TCP_BUFF_SZ
 from helpers import insert_data, unregister_fd
 
 
 def tcp_process_task(client_socket_queue: mp.Queue, measurement_basedir: str, proc_id: int):
-
     client_sockets: Dict[int, socket.socket] = {}
-    file_handles: Dict[int, BinaryIO] = {}
+    client_read_fds = []
     client_identifiers: Dict[int, Dict[str, Any]] = {}
+    file_handles: Dict[int, BinaryIO] = {}
 
     try:
         while True:
@@ -20,19 +23,21 @@ def tcp_process_task(client_socket_queue: mp.Queue, measurement_basedir: str, pr
                 # Registration of New client:
                 # - get client socket
                 # - store client socket in `client_sockets`
-                # - register socket.fileno() to poller
                 # - open file handle for this client
                 new_client: Dict[str: Any] = client_socket_queue.get()
-
                 new_client_socket = new_client["socket"]
+                new_client_addr = new_client["addr"]
+                new_client_port = new_client["port"]
                 new_client_fd = new_client_socket.fileno()
+
                 client_sockets[new_client_fd] = new_client_socket
-                client_identifiers[new_client_fd] = \
-                    {
-                        "addr": new_client["addr"],
-                        "port": new_client["port"],
-                        "transmited": False
-                    }
+                client_read_fds.append(new_client_fd)
+                client_identifiers[new_client_fd] = {
+                    "addr": new_client_addr,
+                    "port": new_client_port,
+                    "transmited": False
+                }
+
                 if new_client_fd not in file_handles.keys():
                     file_handles[new_client_fd] = open(
                         os.path.join(measurement_basedir, f'process_{str(proc_id)}_{new_client_fd}.dat'), 'ab')
@@ -40,19 +45,21 @@ def tcp_process_task(client_socket_queue: mp.Queue, measurement_basedir: str, pr
                     logging.warning(f"The fd: {new_client_fd} already has related file handle")
 
             if len(client_sockets) > 0:
-                for fd in list(client_sockets.keys()):
+                client_read_ready_fds, _, _ = select.select(client_read_fds, [], [])
+                for fd in client_read_ready_fds:
                     client_addr = client_identifiers[fd]['addr']
                     client_port = client_identifiers[fd]['port']
                     data = client_sockets[fd].recv(TCP_BUFF_SZ)
                     if len(data) <= 0:
                         logging.info(f"Client {client_addr}:{client_port} disconnected unexpectedly")
-                        unregister_fd(fd, client_sockets, file_handles)
+                        unregister_fd(fd, client_sockets, client_read_fds, file_handles)
                         continue
 
                     if not client_identifiers[fd]["transmited"]:
                         client_identifiers[fd]["transmited"] = True
                         logging.info(f"Client {client_addr}:{client_port} started sending data")
                     insert_data(file_handles[fd], data)  # TODO: Print info about client
+                    # TODO: Optimize with BytesIO
 
             else:
                 time.sleep(0.01)
