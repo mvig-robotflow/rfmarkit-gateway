44ae25c983c6d7a84b45d543e8b6bd3945b51a7d
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2022-02-10 17:29:43 +0800

    Optimize proc_measurement module

---
+++ proc_measurement/cvt_measurement.py
@@ -0,0 +1,90 @@
+import glob
+import os
+import tqdm
+from typing import List, Dict, Any
+import logging
+
+import numpy as np
+from imu_parser import IMUParser
+
+
+def vectorize_to_np(record_list: List[Dict[str, Any]], keys: List[str]) -> Dict[str, np.ndarray]:
+    """Vectorizing record
+
+    Args:
+        record_list (List[Dict[str, Any]]): List of records, each record is a bundled dictionary
+        keys (List[str]): keys to extract from records
+
+    Returns:
+        Dict[str, np.ndarray]: A dictionary in which keys are desired and values are numpy arrays
+    """
+    assert len(keys) > 0
+    assert len(record_list) > 0
+    res = {}
+    for key in keys:
+        res[key] = np.expand_dims(np.array([record[key] for record in record_list]), axis=-1)
+
+    # Verify length
+    _length: int = len(res[keys[0]])
+    for key in keys:
+        if _length != len(res[key]):
+            raise ValueError("Not every attribute has the same length")
+
+    return res
+
+
+def convert_measurement(measurement_basedir: str, delete_dat: bool = False) -> Dict[str, Dict[str, np.ndarray]]:
+    MEASUREMENT_KEYS: List[str] = [
+        'id', 'timestamp', 'accel_x', 'accel_y', 'accel_z', 'gyro_x', 'gyro_y', 'gyro_z', 'mag_x', 'mag_y', 'mag_z', 'pitch', 'roll', 'yaw', "start_timestamp", "uart_buffer_len"
+    ]
+
+    filenames_list: List[str] = glob.glob(os.path.join(measurement_basedir, '*.dat'))
+    all_measurement: Dict[str, list] = {}
+    all_measurement_np: Dict[str, Dict[str, np.ndarray]] = {}
+    imu_parser = IMUParser()
+
+    # Scatter measurement point
+    for filename in filenames_list:
+        with open(filename, 'rb') as f:
+            points = imu_parser(f)
+            for point in points:
+                point_id = str(point['id'])
+                if point_id not in all_measurement.keys():
+                    all_measurement[point_id] = []
+                all_measurement[point_id].append(point)
+    logging.info(f"Got measurement from {len(all_measurement)} client")
+
+    # Convert to numpy
+    with tqdm.tqdm(len(all_measurement)) as pbar:
+        pbar.update()
+        for key in all_measurement.keys():
+            all_measurement_np[key] = vectorize_to_np(all_measurement[key], MEASUREMENT_KEYS)
+
+    # Clean dat files
+    if delete_dat:
+        map(lambda x: os.remove(x), filenames_list)
+
+    # Dump numpy array
+    for imu_id in all_measurement_np.keys():
+        np.savez(os.path.join(measurement_basedir, f'imu_{imu_id[-6:]}.npz'), **all_measurement_np[imu_id])
+    """example
+    >>> import numpy as np
+    >>> npfile = np.load('./imu_mem_2021-10-21_211859/imu_84f7033b3e78.npz')
+    >>> npfile.files
+    ['id', 'timestamp', 'accel_x', 'accel_y', 'accel_z', 'gyro_x',  'gyro_y', 'gyro_z', 'pitch', 'roll', 'yaw']
+    >>> npfile['timestamp']
+    array([[1.63482358e+15],
+          [1.63482358e+15],
+          [1.63482358e+15],
+          ...,
+          [1.63482235e+15],
+          [1.63482235e+15],
+          [1.63482235e+15]])
+    """
+
+    return all_measurement_np
+
+
+if __name__ == '__main__':
+    res = convert_measurement("C:\\Users\\liyutong\\projectExchange\\imu-interface\\tcpbroker\\imu_data\\imu_mem_2022-01-27_195105")
+    print(res)
--- proc_measurement/imu_parser.py
+++ proc_measurement/imu_parser.py
@@ -2,143 +2,14 @@ import logging
 from typing import List, Dict, BinaryIO
 
 import numpy as np
-import ctypes
 import struct
-
-
-class ch_imu_data_t(ctypes.Structure):
-    _fields_ = [
-        ("id", ctypes.c_uint32),
-        ("acc", ctypes.c_float * 3),
-        ("gyr", ctypes.c_float * 3),
-        ("mag", ctypes.c_float * 3),
-        ("eul", ctypes.c_float * 3),
-        ("quat", ctypes.c_float * 4),
-        ("timestamp", ctypes.c_uint32)
-    ]
-
-
-ch_imu_data_t_fmt = "I3f3f3f3f4fI"
-hi229_dgram_meta_t_fmt = "q12sBqi"
-
-logging.basicConfig(level=logging.INFO)
-
-DATA_FORMAT = {
-    "accel_x": [5, 4],
-    "accel_y": [7, 6],
-    "accel_z": [9, 8],
-    "gyro_x": [11, 10],
-    "gyro_y": [13, 12],
-    "gyro_z": [15, 14],
-    "roll": [17, 16],
-    "pitch": [19, 18],
-    "yaw": [21, 20],
-    "temp": [24, 23],
-    "mag_x": [26, 25],
-    "mag_y": [28, 27],
-    "mag_z": [30, 29],
-}
-META_FORMAT = {
-    "data": [0, 32],
-    "timestamp": [32, 40],
-    "id": [40, 52],
-    "gy_scale": [52, 53],
-    "start_timestamp": [53, 61],
-    "uart_buffer_len": [61, 65],
-}
-DATA_DIVIDER = {
-    "accel_x": 4 / 65536,
-    "accel_y": 4 / 65536,
-    "accel_z": 4 / 65536,
-    "gyro_x": 500 / 65536,
-    "gyro_y": 500 / 65536,
-    "gyro_z": 500 / 65536,
-    "roll": 1 / 100,
-    "pitch": 1 / 100,
-    "yaw": 1 / 100,
-    "temp": 1 / 100,
-    "mag_x": 4 / 65536,
-    "mag_y": 4 / 65536,
-    "mag_z": 4 / 65536,
-}
-
-
-# def extract_meta(data):
-#     """Extract meta info from data
-#
-#     Args:
-#         data ([type]): [description]
-#     """
-#     timestamp: float = sum([data[idx] * 0x100 ** (idx)
-#                             for idx in range(0,8)]) / 1e6  # Already the compensated time
-#     device_id: str = ''.join([chr(data[idx]) for idx in range(8, 20)])
-#     scale = data[20]  # '0x8b'
-#     start_timestamp = sum(
-#         [data[idx] * 0x100 ** (idx - 21) for idx in
-#          range(21,29)]) / 1e6
-#
-#     uart_buffer_len = sum(
-#         [data[idx] * 0x100 ** (idx - 29) for idx in
-#          range(29, 33)])
-#
-#     return timestamp, device_id, scale, start_timestamp, uart_buffer_len
-
-def extract_meta(data):
-    """Extract meta info from data
-
-    Args:
-        data ([type]): [description]
-    """
-    timestamp: float = data[0]# Already the compensated time
-    device_id: str = ''.join([chr(data[1][idx]) for idx in range(len(data[1]))])
-    scale = data[2]  # '0x8b'
-    start_timestamp = data[3]
-    uart_buffer_len = data[4]
-
-    return timestamp, device_id, scale, start_timestamp, uart_buffer_len
-
-def parse_data(data):
-    res: dict = {
-        "accel_x": 0.0,
-        "accel_y": 0.0,
-        "accel_z": 0.0,
-        "gyro_x": 0.0,
-        "gyro_y": 0.0,
-        "gyro_z": 0.0,
-        "roll": 0.0,
-        "pitch": 0.0,
-        "yaw": 0.0,
-        "temp": 0.0,
-        "mag_x": 0.0,
-        "mag_y": 0.0,
-        "mag_z": 0.0,
-    }
-
-    timestamp, id, scale, start_timestamp, uart_buffer_len = extract_meta(data['meta_data'])
-
-    res["accel_x"] = data['imu_data'][1]
-    res["accel_y"] = data['imu_data'][2]
-    res["accel_z"] = data['imu_data'][3]
-    res["gyro_x"] = data['imu_data'][4]
-    res["gyro_y"] = data['imu_data'][5]
-    res["gyro_z"] = data['imu_data'][6]
-    res["roll"] = data['imu_data'][10]
-    res["pitch"] = data['imu_data'][11]
-    res["yaw"] = data['imu_data'][12]
-    res["mag_x"] = data['imu_data'][7]
-    res["mag_y"] = data['imu_data'][8]
-    res["mag_z"] = data['imu_data'][9]
-
-    res["timestamp"] = timestamp
-    res["id"] = id
-    res["start_timestamp"] = start_timestamp
-    res["uart_buffer_len"] = uart_buffer_len
-    return res
 
 
 class IMUParser:
     ADDR = 0xe5
     BLOCK_SZ: int = 0x1000
+    ch_imu_data_t_fmt = "I3f3f3f3f4ffI"
+    hi229_dgram_meta_t_fmt = "=q12sBqi"
 
     def __init__(self) -> None:
         self.buf: np.ndarray = np.zeros(shape=(100), dtype=np.uint8)
@@ -156,32 +27,40 @@ class IMUParser:
         self.meta_valid = False
         self.buf.fill(0)
 
-    # TODO: There are still room of optimization
     def _sync(self, read_buf: bytes):
+        """
+        Magic Sync Algorithm.
+        See imu-esp-node (C code)
+        Args:
+            read_buf:
+
+        Returns:
+
+        """
         res: List[Dict[str, float]] = []
         if len(read_buf) <= 0:
-            # TODO: Think about what we should do with an empty file
+            logging.warning('Empty record!!!')
             return res
 
         for idx in range(len(read_buf)):
             if read_buf[idx] == self.ADDR:
                 _start_idx = idx + 1
-                _end_idx = _start_idx + struct.calcsize(ch_imu_data_t_fmt)
-                data_valid = sum(read_buf[_start_idx: _end_idx]) % 0x100 == read_buf[_end_idx + 1]
+                _end_idx = _start_idx + struct.calcsize(self.ch_imu_data_t_fmt)
+                data_valid = sum(read_buf[_start_idx: _end_idx]) % 0x100 == read_buf[_end_idx]
                 if data_valid:
-                    _end_idx = _start_idx + 1 + struct.calcsize(ch_imu_data_t_fmt) + struct.calcsize(
-                        hi229_dgram_meta_t_fmt)
-                    data_valid = sum(read_buf[idx: _end_idx]) % 0x100 == read_buf[_end_idx + 1]
+                    _end_idx = _start_idx + 1 + struct.calcsize(self.ch_imu_data_t_fmt) + struct.calcsize(
+                        self.hi229_dgram_meta_t_fmt)
+                    data_valid = sum(read_buf[idx: _end_idx]) % 0x100 == read_buf[_end_idx]
                     if data_valid:
                         return {
                             "sync_start_idx": _start_idx - 1,
-                            "dgram_length": _end_idx - _start_idx + 3,
+                            "dgram_length": _end_idx - _start_idx + 2,
                             "imu_offset": 1,
-                            "imu_length": struct.calcsize(ch_imu_data_t_fmt),
-                            "imu_fmt": ch_imu_data_t_fmt,
-                            "meta_offset": _start_idx + 2 + struct.calcsize(ch_imu_data_t_fmt),
-                            "meta_length": struct.calcsize(hi229_dgram_meta_t_fmt),
-                            "meta_fmt": hi229_dgram_meta_t_fmt
+                            "imu_length": struct.calcsize(self.ch_imu_data_t_fmt),
+                            "imu_fmt": self.ch_imu_data_t_fmt,
+                            "meta_offset": _start_idx + 1 + struct.calcsize(self.ch_imu_data_t_fmt),
+                            "meta_length": struct.calcsize(self.hi229_dgram_meta_t_fmt),
+                            "meta_fmt": self.hi229_dgram_meta_t_fmt
                         }
                 else:
                     continue
@@ -190,34 +69,49 @@ class IMUParser:
 
         return None
 
-    def _parse(self, read_buf, fmt):
-        res: List[Dict] = []
+    def _parse(self, read_buf, fmt) -> List[Dict]:
+        result: List[Dict] = []
         start_idx = fmt['sync_start_idx']
         try:
             while start_idx + fmt['dgram_length'] < len(read_buf):
-                imu_data = struct.unpack(ch_imu_data_t_fmt, read_buf[start_idx + fmt['imu_offset']:start_idx + fmt['imu_offset'] + fmt['imu_length']])
-                meta_data = struct.unpack(hi229_dgram_meta_t_fmt, read_buf[start_idx + fmt['meta_offset']:start_idx + fmt['meta_offset'] + fmt['meta_length']])
-                res.append(parse_data({"imu_data": imu_data, "meta_data": meta_data}))
+                imu_data = struct.unpack(self.ch_imu_data_t_fmt, read_buf[start_idx + fmt['imu_offset']:start_idx + fmt['imu_offset'] + fmt['imu_length']])
+                meta_data = struct.unpack(self.hi229_dgram_meta_t_fmt, read_buf[start_idx + fmt['meta_offset']:start_idx + fmt['meta_offset'] + fmt['meta_length']])
+
+                imu_dict: dict = {"accel_x": imu_data[1], "accel_y": imu_data[2], "accel_z": imu_data[3], "gyro_x": imu_data[4], "gyro_y": imu_data[5], "gyro_z": imu_data[6],
+                                  "roll": imu_data[10],
+                                  "pitch": imu_data[11], "yaw": imu_data[12], "temp": 0.0, "mag_x": imu_data[7], "mag_y": imu_data[8], "mag_z": imu_data[9]}
+
+                meta_dict = {'timestamp': meta_data[0],
+                             'id': ''.join([chr(meta_data[1][idx]) for idx in range(len(meta_data[1]))]),
+                             'scale': meta_data[2],
+                             'start_timestamp': meta_data[3],
+                             'uart_buffer_len': meta_data[4]}
+
+                result.append({**imu_dict, **meta_dict})
 
                 start_idx += fmt['dgram_length']
         except Exception as e:
+            print(e)
             pass
 
-        return res
+        return result
 
     def __call__(self, f: BinaryIO) -> List[Dict[str, float]]:
         read_buf = f.read()
+        if len(read_buf) <= 0:
+            logging.warning("Empty recording")
+            return []
         fmt = self._sync(read_buf)
-        res = self._parse(read_buf, fmt)
+        result = self._parse(read_buf, fmt)
 
-        return res
+        return result
 
 
 if __name__ == '__main__':
-    gy = IMUParser()
+    parser = IMUParser()
     with open(
-            "C:\\Users\\liyutong\\projectExchange\\imu-interface\\tcp_broker\\imu_data\\imu_mem_2022-01-27_195105\\process_0_384.dat",
+            "/Users/liyutong/projectExchange/imu-interface/tcpbroker/imu_data/imu_mem_2022-02-09_235726/process_0_11.dat",
             'rb') as f:
-        res = gy(f)
+        res = parser(f)
 
     print(res)
